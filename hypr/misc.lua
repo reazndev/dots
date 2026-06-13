5732cf4c4a7964ab0dfac39633a2da2d73c144f5
reazndev <ruby.florian@proton.me>
2026-06-13 13:48:01 +0200

    add umlaute and caps -> super rebind

--- hypr/misc.lua
+++ hypr/misc.lua
@@ -23,9 +23,9 @@ hl.config({
     -- https://wiki.hypr.land/Configuring/Variables/#input
     input = {
         kb_layout = "us",
-        kb_variant = "",
+        kb_variant = "cz_sk_de",
         kb_model = "",
-        kb_options = "",
+        kb_options = "caps:super",
         kb_rules = "",
         follow_mouse = 1,
         sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
@@ -40,4 +40,3 @@ hl.config({
         force_zero_scaling = true,
     },
 })
-
